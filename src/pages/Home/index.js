65be4fe606a2820a6f27f4a5c033938f87dd1742
HongLin Tian <thl384458870@outlook.com>
2020-09-24 06:33:47 +0000

    feat: topic name

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import './index.less';
 import Pannel from '../../components/Pannel';
+import classNames from 'classnames';
 
 function Index() {
+  const [tab, setTab] = React.useState('全部');
+
+  const tabs = [
+    { name: '全部' },
+    { name: '精华' },
+    { name: '分享' },
+    { name: '问答' },
+    { name: '招聘' },
+    { name: '客户端测试' },
+  ];
+
+  function h(tabName) { setTab(tabName); }
+
+  const header = tabs.map(({ name }) => {
+    const cls = classNames('tab', { active: name == tab });
+    return (
+      <span
+        onClick={() => h(name)}
+        className={cls}
+        key={name}>
+          {name}
+      </span>
+    )
+  })
+
   return (
     <div className="home-page">
-      <div style={{ width: 360 }}>
-        <Pannel title="无人回复的话题">
-          <div>12312</div>
-          <div>12312</div>
-          <div>12312</div>
-          <div>12312</div>
-        </Pannel>
-        <Pannel title="积分榜">
-          <div>12312</div>
-          <div>12312</div>
-          <div>12312</div>
-          <div>12312</div>
-        </Pannel>
-        <Pannel>
-          <h4 style={{ marginBottom: 12 }}>Cnode 专业社区</h4>
-          <div>12312</div>
-          <div>12312</div>
-          <div>12312</div>
-          <div>12312</div>
-        </Pannel>
-      </div>
+      <Pannel title={header}>
+        <div>12312</div>
+        <div>12312</div>
+        <div>12312</div>
+        <div>12312</div>
+      </Pannel>
     </div>
   );
 }
