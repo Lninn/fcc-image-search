5dd869e5edc793f80989dc773e02e9ed97b1111c
Lninn <thl384458870@outlook.com>
2020-09-26 13:49:42 +0800

    feat: add login form

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import Panel from '@components/panel';
 import PagePanelTitle from '@components/page-panel-title';
+import './index.less';
+
+function LoginForm() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className='login-from'>
+      <div className='form-control'>
+        <div className='form-label'>
+          <label>用户名</label>
+        </div>
+        <div className="form-input">
+          <input type='text' />
+        </div>
+      </div>
+      <div className='form-control'>
+        <div className='form-label'>
+          <label>密码</label>
+        </div>
+        <div className="form-input">
+          <input type='text' />
+        </div>
+      </div>
+      <div className='form-control'>
+        <button>登录</button>
+      </div>
+    </form>
+  );
+}
 
 function Index() {
   return (
     <div className="signin-page">
       <Panel header={<PagePanelTitle pageTitle='登陆' />}>
-        <p>
-          登陆
-        </p>
+        <LoginForm />
       </Panel>
     </div>
   );
